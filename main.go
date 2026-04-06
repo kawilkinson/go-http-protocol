38b527e82d86932eb303c034a15c4ea78e55cb27
Kyle Wilkinson <kyle.wilkinson.tech@gmail.com>
2026-04-06 16:08:27 -0400

    have main read txt and decouple lines 8 bytes at a time

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	currLine := ""
+	for {
+		var bytes [8]byte
+		n, err := file.Read(bytes[:])
+		if err != nil {
+			if currLine != "" {
+				fmt.Printf("read: %s\n", currLine)
+				currLine = ""
+			}
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			fmt.Println("Error reading file:", err)
+			break
+		}
+
+		currBytes := string(bytes[:n])
+		parts := strings.Split(currBytes, "\n")
+		for i, part := range parts {
+			if i < len(parts)-1 {
+				fmt.Printf("read: %s%s\n", currLine, part)
+				currLine = ""
+			}
+		}
+		currLine += parts[len(parts)-1]
+	}
 }
